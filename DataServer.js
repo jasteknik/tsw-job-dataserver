e4b785b56f74f248e69cd202fd624076b540d058
jasteknik <sojaan00@gmail.com>
2022-04-05 07:52:01 +0300

    heroku deployment

--- DataServer.js
+++ DataServer.js
@@ -42,7 +42,7 @@ app.post('/loadQuery', (req, res) => {
   response.then(timeTableReady => res.json(timeTableReady))
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
